408b2938b6f037e91c58d1544c2f73fb93c5cd0c
Lixuan Yang <ly2555@columbia.edu>
2022-12-13 16:27:51 -0500

    frontend update

--- js/courseteacher.js
+++ js/courseteacher.js
@@ -27,3 +27,34 @@ function getPie(){
       });
     document.getElementById("showpie").style.display="none";
 }
+
+
+function getqrstring(){
+    var teacherinfo = {
+        'course_name': "test"
+    };
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        // data: JSON.stringify([{
+        //     course_name: "321"
+        // }]),
+        data: JSON.stringify(teacherinfo),
+        url: "https://p9c9ep74ye.execute-api.us-east-1.amazonaws.com/test-stage/course/teacher/generate_qr",
+        success: function(data){
+            console.log('success')
+            console.log(data);
+        },
+        error: function(data){
+            console.log('error')
+            console.log(data);
+        }
+    })
+
+
+}
+
+        
+
