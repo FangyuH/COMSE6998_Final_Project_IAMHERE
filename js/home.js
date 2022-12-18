eda4b5879090baee5d8152ec5b0a0c0a444d99c1
Lixuan Yang <ly2555@columbia.edu>
2022-12-18 17:52:25 -0500

    frontend update

--- js/home.js
+++ js/home.js
@@ -15,18 +15,25 @@ $(document).ready(function(){
             data: JSON.stringify(courseinfo),
             url: "https://rlofxcp9dd.execute-api.us-east-1.amazonaws.com/beta/course/create_course",
             success: function(data){
-                console.log(data['statusCode'], data);
+                console.log("flag:", data);
+                sessionStorage.setItem("createCourse", $("#courseid").val());
                 //$("#feedback").html(data['body'])
                 if(data['statusCode'] == 200){
                     setTimeout(function(){
                         window.location="home.html";
-                    },3000);
+                    },1000);
                 };
-                document.getElementById('createcourses').innerHTML = $("#courseid").val();
+                document.getElementById('createCoursesName').innerHTML = $("#courseid").val();
             },
             error: function(data){
-                console.log(data['statusCode'], data);
-                document.getElementById('createcourses').innerHTML = $("#courseid").val();
+                console.log("data:", data);
+                sessionStorage.setItem("createCourse", $("#courseid").val());
+                setTimeout(function(){
+                    alert('course created successfully!')
+                    console.log("create course successfully");
+                    document.getElementById('createCoursesName').innerHTML = $("#courseid").val();
+                },1000);
+                
             }
         })
     //document.getElementById('create').style.display='none'
--- js/register.js
+++ js/register.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function(){
     
     $("#submit").click(function(){
@@ -35,3 +36,81 @@ $(document).ready(function(){
     });
 
 });
+*/
+function register(){
+    setTimeout(() => {
+    userid = $("#email").val();
+    var apigClient = apigClientFactory.newClient();
+    // var reader = new FileReader();
+    var file = document.getElementById('upload_face').files[0];
+    file.constructor = () => file;
+
+    console.log('File : ', file);
+    //document.getElementById('uploaded_file').value = "";
+    var filePath = (document.getElementById('upload_face').value).split("\\");
+    var fileName = filePath[filePath.length - 1];
+    console.log("imagename:",fileName);
+    if ((filePath == "") || (!['png', 'jpg', 'jpeg'].includes(fileName.split(".")[1]))) {
+        alert("Please upload a valid .png/.jpg/.jpeg file!");
+    } else {
+
+        var params = {
+            "folder":"lixuanyanghw2",
+            "item": userid,
+            "Content-Type":file.type,
+            'x-amz-meta-customLabels':'custom_labels.value'
+            //'Access-Control-Allow-Origin':"*"
+        };
+        console.log("Content-Type:",file.type)
+        var additionalParams = {
+        };
+
+        apigClient.uploadFolderItemPut(params, file, additionalParams)
+            .then(function(result) {
+                //API Call Success
+                console.log("bucket success:",JSON.stringify(result));
+                
+
+            }).catch(function(result) {
+                // API Call Failed
+                console.log(JSON.stringify(result));
+            });
+
+    }
+
+    var profile = {
+        email: $("#email").val(),
+        username: $("#username").val(),
+        password: $("#password").val(),
+        bucket: 'lixuanyanghw2',
+        photo: email+'.'+file.type
+    };      
+    console.log("send data:", profile);
+
+    //send face photo info
+    $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType:"json",
+        data: JSON.stringify({
+            email: $("#email").val(),
+            username: $("#username").val(),
+            password: $("#password").val(),
+            bucket: 'lixuanyanghw2',
+            photo: email+'.'+file.type
+        }),
+        url: "https://rlofxcp9dd.execute-api.us-east-1.amazonaws.com/beta/user/create_user",
+        success: function(data){
+            console.log('success:',data)
+            if(data['status'] == 200){
+                let email = $("#email").val();
+                sessionStorage.setItem("email", email);
+                console.log(sessionStorage.getItem("email"));
+                setTimeout(function(){
+                    window.location="home.html";
+                },1000);
+            };
+        }
+    });
+}, 2000);
+}
