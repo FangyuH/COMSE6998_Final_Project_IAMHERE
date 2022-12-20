cc122edeceb2fde2e6925446d77012f5f79f5ec1
FangyuH <101750987+FangyuH@users.noreply.github.com>
2022-12-20 15:37:50 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -10,12 +10,15 @@ $(document).ready(function(){
         $.ajax({
             type: "POST",
             contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(login),
+            data: JSON.stringify({
+                email: $("#email").val(),
+                password: $("#password").val(),
+            }),
             url: "https://rlofxcp9dd.execute-api.us-east-1.amazonaws.com/beta/login",
             success: function(data){
                 console.log('success', data);
                 $("#feedback").html(data['body'])
-                if (data['status'] == 200){
+                if (data['statusCode'] == 200){
                     let email = $("#email").val();
                     sessionStorage.setItem("email", email);
                     console.log(sessionStorage.getItem("email"));
@@ -28,11 +31,7 @@ $(document).ready(function(){
                 };
             },
             error: function(data){     
-                let email = $("#email").val();
-                sessionStorage.setItem("email", email);
-                setTimeout(function(){
-                window.location="home.html";
-                },1000);   
+                alert("wrong password");
             }
         })
     });
