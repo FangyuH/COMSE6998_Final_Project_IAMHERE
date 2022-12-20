15d059fc1db3f186207982dc844bc8ffc2df8ae2
FangyuH <101750987+FangyuH@users.noreply.github.com>
2022-12-20 15:37:27 -0500

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,49 +1,3 @@
-/*
-$(document).ready(function(){
-    
-    $("#submit").click(function(){
-        
-        var profile = {
-                email: $("#email").val(),
-                username: $("#username").val(),
-                password: $("#password").val(),
-                photo: $("#photo").val(),
-        };
-        
-        console.log(profile)
-
-        $.ajax({
-            type: "POST",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(profile),
-            url: "https://rlofxcp9dd.execute-api.us-east-1.amazonaws.com/beta/user/create_user",
-            success: function(data){
-                console.log('success', data);
-                $("#feedback").html(data['body'])
-                if(data['statusCode'] == 200){
-                    let email = $("#email").val();
-                    sessionStorage.setItem("email", email);
-                    console.log(sessionStorage.getItem("email"));
-                    setTimeout(function(){
-                        window.location="home.html";
-                      },1000);
-                };
-            },
-            
-            error: function(data){
-                let email = $("#email").val();
-                sessionStorage.setItem("email", email);
-                console.log(sessionStorage.getItem("email"));
-                setTimeout(function(){
-                    window.location="home.html";
-                    },1000);
-                console.log('Error', data);
-            }
-        })
-    });
-
-});
-*/
 function register(){
     setTimeout(() => {
     userid = $("#email").val();
@@ -109,7 +63,7 @@ function register(){
         url: "https://rlofxcp9dd.execute-api.us-east-1.amazonaws.com/beta/user/create_user",
         success: function(data){
             console.log('success:',data)
-            if(data['status'] == 200){
+            if(data['statusCode'] == 200){
                 let email = $("#email").val();
                 sessionStorage.setItem("email", email);
                 console.log(sessionStorage.getItem("email"));
